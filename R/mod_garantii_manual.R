c3cfc1f4f1adbb69a12ae05b2937b36911c65a80
Marius Tita <marius.tita81@gmail.com>
2022-04-19 17:03:42 +0300

    Improve save button position UI

--- R/mod_garantii_manual.R
+++ R/mod_garantii_manual.R
@@ -15,26 +15,29 @@ mod_garantii_manual_ui <- function(id){
     hr(),
     column(width = 12,h4("Se completeaza manual datele Prima Casa, OUG37 si Investeste in tine.")),
     br(), br(), hr(),
-    column(width = 4, selectInput(inputId = ns("select_program"),
+    column(width = 3, selectInput(inputId = ns("select_program"),
                                          label = "Selecteaza programul",
                                          choices = c("Prima Casa","Investeste in tine", "OUG37"))),
-           column(width = 4, selectInput(inputId = ns("data_solduri_manuale"),
+           column(width = 3, selectInput(inputId = ns("data_solduri_manuale"),
                                          label = "Selecteaza data soldurilor",choices = c()) ),
-           column(width = 4, br(), 
+           column(width = 6, br(), 
                   actionButton(inputId = ns("save_solduri_manuale"), icon = icon("save"),
-                                label = "Salveaza datele introduse mai jos")),
+                      label = "Salveaza datele introduse mai jos",
+                      style = "padding-top: 10px; margin-left: 100px; border-color: #ff007b;")),
            column(width = 3, shinyWidgets::autonumericInput(inputId = ns("sold_garantii_input"),
                                   label = "Soldul garantiilor, lei", value = 0,align = "right",
                                   digitGroupSeparator = ",",decimalPlaces = 2,minimumValue = ,
-                                  decimalCharacter = "."  )),
+                                  decimalCharacter = ".",modifyValueOnWheel = FALSE  )),
            column(width = 3, shinyWidgets::autonumericInput(ns("nr_contracte_input"),
                                   digitGroupSeparator = ",",decimalPlaces = 0, value = 0,align = "right",
-                                  label = "Numarul de contracte")),
+                                  label = "Numarul de contracte",modifyValueOnWheel = FALSE)),
            column(width = 3, shinyWidgets::autonumericInput(ns("nr_beneficiari_input"),
-                                  label = "Numarul de beneficiari", decimalPlaces = 0, value = 0,align = "right")),
+                                  label = "Numarul de beneficiari", decimalPlaces = 0, value = 0,align = "right",
+                                  modifyValueOnWheel = FALSE)),
            column(width = 3, shinyWidgets::autonumericInput(inputId = ns("sold_credit_input"),
                                   label = "Soldul creditelor, lei", value = 0,align = "right",
-                                  digitGroupSeparator = ",",decimalPlaces = 2,minimumValue = 0,decimalCharacter = "."))
+                                  digitGroupSeparator = ",",decimalPlaces = 2,minimumValue = 0,decimalCharacter = ".",
+                                  modifyValueOnWheel = FALSE))
   )
   )  
   
@@ -68,7 +71,7 @@ mod_garantii_manual_server <- function(id, vals){
                                     input$nr_beneficiari_input != 0,
                                    input$sold_credit_input != 0 )
         
-         data.frame(Tip_surse = "Nume_cont_stat", "Tip fonduri" = input$select_program,check.names = FALSE,
+         data.frame( Tip_surse = "Nume_cont_stat", "Tip fonduri" = input$select_program,check.names = FALSE,
                    Nr_contracte = input$nr_contracte_input, Nr_beneficiari = input$nr_beneficiari_input,
                    Sold_garantii = input$sold_garantii_input, 
                    data_raport = as.Date.character(input$data_solduri_manuale),
@@ -77,10 +80,7 @@ mod_garantii_manual_server <- function(id, vals){
       
       vals_manual$finish_update <- janitor::compare_df_cols_same(df_manual(),vals$view_baza_solduri)
       
-      
-      
       if (vals_manual$finish_update) {
-       
        
         vals$view_baza_solduri <- dplyr::bind_rows(df_manual(), vals$view_baza_solduri %>% 
                 dplyr::mutate(temp_column = ifelse(`Tip fonduri` == input$select_program & 
@@ -101,7 +101,7 @@ mod_garantii_manual_server <- function(id, vals){
         
        }
       
-      else {shinyFeedback::showToast(type = "error",title = "ERROR",message = "Failed to save", keepVisible = F) }
+      else { shinyFeedback::showToast(type = "error",title = "ERROR",message = "Failed to save", keepVisible = F) }
       
     })
     
